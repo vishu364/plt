0fba9bb4f3cc5662932983aefc4528e33eb5b3bc
sromano87 <siromano@unisa.it>
2024-10-15 12:13:06 +0200

    Add source files

---
+++ piglatin.py
@@ -0,0 +1,10 @@
+class PigLatin:
+
+    def __init__(self, phrase: str):
+        pass
+
+    def get_phrase(self) -> str:
+        pass
+
+    def translate(self) -> str:
+        pass
---
+++ test/test_piglatin.py
@@ -0,0 +1,9 @@
+import unittest
+from piglatin import PigLatin
+from error import PigLatinError
+
+
+class TestPigLatin(unittest.TestCase):
+
+    def test_something(self):
+        pass
